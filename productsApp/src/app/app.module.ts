be9551cc31cd48752d067afcd7c8abbb407ba725
Andrej492 <andrejmatovic1@gmail.com>
2022-03-04 18:02:07 +0100

    added product-create and product-list component to products

--- productsApp/src/app/app.module.ts
+++ productsApp/src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { HeaderComponent } from './header/header.component'
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductCreateComponent } from './products/product-create/product-create.component';
+import { ProductListComponent } from './products/product-list/product-list.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductCreateComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +26,8 @@ import { HeaderComponent } from './header/header.component'
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
